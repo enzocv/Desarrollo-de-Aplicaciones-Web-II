89f76c74ad97b8c3e3562510389ab4dcbfa62bd0
Enzo Catalán Vargas <enzocv96@gmail.com>
2018-09-03 13:50:05 -0500

    Ejercicio 7

---
+++ Ejer_Propuestos1_Catalan/Ejer_Propuestos1_Catalan/Controllers/Ejercicio7Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ejer_Propuestos1_Catalan.Models;
+
+namespace Ejer_Propuestos1_Catalan.Controllers
+{
+    public class Ejercicio7Controller : Controller
+    {
+        // GET: Ejercicio7
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dibujar(ClsEjercicio7 objdibujo)
+        {
+            objdibujo.grafico = new List<String>();
+
+            for (int i = 0; i < objdibujo.filas; i++)
+            {
+                string x = "";
+                for (int j = 0; j <= i; j++)
+                {
+                    x = x + objdibujo.simbolo;
+                }
+                objdibujo.grafico.Add(x);
+            }
+
+            return View("Dibujar", objdibujo);
+        }
+    }
+}
---
+++ Ejer_Propuestos1_Catalan/Ejer_Propuestos1_Catalan/Models/ClsEjercicio7.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ejer_Propuestos1_Catalan.Models
+{
+    public class ClsEjercicio7
+    {
+        public string simbolo { get; set; }
+        public string alinear { get; set; }
+        public int filas { get; set; }
+        public List<String> grafico { get; set; }
+    }
+}
