f55b5b7f671c08b51e7ae9d58b34e38575c95ac4
Enzo Catalán Vargas <enzocv96@gmail.com>
2018-09-02 22:13:25 -0500

    Ejercicio 6

---
+++ Ejer_Propuestos1_Catalan/Ejer_Propuestos1_Catalan/Controllers/Ejercicio6Controller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ejer_Propuestos1_Catalan.Models;
+
+namespace Ejer_Propuestos1_Catalan.Controllers
+{
+    public class Ejercicio6Controller : Controller
+    {
+        // GET: Ejercicio6
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Calcular(ClsEjercicio6 objcoche)
+        {
+            objcoche.total = 0;
+            switch (objcoche.coche)
+            {
+                case "chico":
+                    objcoche.total = (20 * objcoche.km) + (15 * objcoche.dias);
+                    break;
+                case "mediano":
+                    objcoche.total = (30 * objcoche.km) + (20 * objcoche.dias);
+                    break;
+                case "grande":
+                    objcoche.total = (40 * objcoche.km) + (30 * objcoche.dias);
+                    break;
+                default:
+                    objcoche.total = 0;
+                    break;
+            }
+
+            return View("Calcular", objcoche);
+        }
+    }
+}
---
+++ Ejer_Propuestos1_Catalan/Ejer_Propuestos1_Catalan/Models/ClsEjercicio6.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ejer_Propuestos1_Catalan.Models
+{
+    public class ClsEjercicio6
+    {
+        public string coche { get; set; }
+        public double km { get; set; }
+        public int dias { get; set; }
+        public double total { get; set; }
+    }
+}
