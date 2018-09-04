bd3118d2235ba274cbacfdcba47604b9c06b66bf
Enzo Catalán Vargas <enzocv96@gmail.com>
2018-09-04 16:34:36 -0500

    Ejercicio 10

---
+++ Ejer_Propuestos1_Catalan/Ejer_Propuestos1_Catalan/Controllers/Ejercicio10Controller.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ejer_Propuestos1_Catalan.Models;
+
+namespace Ejer_Propuestos1_Catalan.Controllers
+{
+    public class Ejercicio10Controller : Controller
+    {
+        // GET: Ejercicio10
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dibujar(ClsEjercicio10 objbrujula)
+        {
+            /*Si x> 0 y y> 0 = Se encuentra en el Norte
+            Si x > 0 y y<0 = Se encuentra en el Sur
+            Si x < 0 y y<0 = Se encuentra en el Este
+            Si x < 0 y y> 0 = Se encuentra en el Oeste
+            Caso contrario se encuentra en el origen*/
+
+            if (objbrujula.posX > 0 && objbrujula.posY > 0)
+            {
+                objbrujula.ubicacion = "Norte";
+            }
+            else if (objbrujula.posX > 0 && objbrujula.posY < 0)
+            {
+                objbrujula.ubicacion = "Sur";
+            }
+            else if (objbrujula.posX < 0 && objbrujula.posY < 0)
+            {
+                objbrujula.ubicacion = "Este";
+            }
+            else if (objbrujula.posX < 0 && objbrujula.posY > 0)
+            {
+                objbrujula.ubicacion = "Oeste";
+            }
+            else
+            {
+                //centro posX = 0 y posY = 0
+                objbrujula.ubicacion = "Origen";
+            }
+
+            return View("Dibujar", objbrujula);
+        }
+    }
+}
---
+++ Ejer_Propuestos1_Catalan/Ejer_Propuestos1_Catalan/Models/ClsEjercicio10.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ejer_Propuestos1_Catalan.Models
+{
+    public class ClsEjercicio10
+    {
+        public int posX { get; set; }
+        public int posY { get; set; }
+        public string ubicacion { get; set; }
+    }
+}
