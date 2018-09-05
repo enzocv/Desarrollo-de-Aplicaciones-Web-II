ee47cf275d9004c36ca79d7af252006368ce400e
Enzo Catalán Vargas <enzocv96@gmail.com>
2018-09-05 10:16:31 -0500

    Ejercicio 2

---
+++ Ejer_Propuestos1_Catalan/Ejer_Propuestos1_Catalan/Controllers/Ejercicio2Controller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ejer_Propuestos1_Catalan.Models;
+
+namespace Ejer_Propuestos1_Catalan.Controllers
+{
+    public class Ejercicio2Controller : Controller
+    {
+        // GET: Ejercicio2
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CalcularNumeros(ClsEjercicio2 objejer2)
+        {
+            objejer2.valPositivo = new List<int>();
+            objejer2.valNegativo = new List<int>();
+            IList<ClsEjercicio2> List = new List<ClsEjercicio2>();
+            if (objejer2.numeros != null && ModelState.IsValid)
+            {
+                foreach (int i in objejer2.numeros)
+                    if (i < 0)
+                    {
+                        objejer2.valNegativo.Add(i);
+                    }
+                    else
+                    {
+                        objejer2.valPositivo.Add(i);
+                    }
+                ViewData["numNegativos"] = objejer2.valNegativo;
+                ViewData["numPositivos"] = objejer2.valPositivo;
+                objejer2.validar = null;
+            }
+            else
+            {
+                objejer2.validar = "ERROR";
+                ViewBag.Comunicado = "Ingrese valor válido";
+            }
+
+            return View(objejer2);
+        }
+    }
+}
---
+++ Ejer_Propuestos1_Catalan/Ejer_Propuestos1_Catalan/Models/ClsEjercicio2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ejer_Propuestos1_Catalan.Models
+{
+    public class ClsEjercicio2
+    {
+        [Required]
+        public int[] numeros { get; set; }
+        public int valor { get; set; }
+        public string validar { get; set; }
+        public List<int> valNegativo { get; set; }
+        public List<int> valPositivo { get; set; }
+    }
+}
