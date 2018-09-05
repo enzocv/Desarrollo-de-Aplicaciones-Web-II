32f285ebbce0acbee68a7138171f178560970324
Enzo Catalán Vargas <enzocv96@gmail.com>
2018-09-05 12:19:33 -0500

    Ejercicio 11

---
+++ Ejer_Propuestos1_Catalan/Ejer_Propuestos1_Catalan/Controllers/Ejercicio11Controller.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ejer_Propuestos1_Catalan.Models;
+
+namespace Ejer_Propuestos1_Catalan.Controllers
+{
+    public class Ejercicio11Controller : Controller
+    {
+        // GET: Ejercicio11
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CalcularUllman(ClsEjercicio11 objEjercicio11)
+        {
+            objEjercicio11.Uentero = new List<int>();
+            objEjercicio11.Usimbolo = new List<string>();
+            ViewBag.Primero = objEjercicio11.entero;
+            if (objEjercicio11.entero != 0 && ModelState.IsValid)
+            {//Validando
+                objEjercicio11.Uentero.Add(objEjercicio11.entero);
+                do
+                {
+                    if (objEjercicio11.entero % 2 == 0)
+                    {
+                        objEjercicio11.entero = objEjercicio11.entero / 2;
+                        objEjercicio11.Uentero.Add(objEjercicio11.entero);
+                    }
+                    else
+                    {
+                        objEjercicio11.entero = (objEjercicio11.entero * 3) + 1;
+                        objEjercicio11.Uentero.Add(objEjercicio11.entero);
+                    }
+                } while (objEjercicio11.entero != 1);
+                
+            }
+            
+            //generar el grafico segun el resultado de Ullman
+            for (int i = 0; i < objEjercicio11.Uentero.Count(); i++)
+            {
+                string sim = "";
+                for (int j = 0; j < objEjercicio11.Uentero[i]; j++)
+                {
+                    sim = sim + objEjercicio11.simbolo;
+                }
+                objEjercicio11.Usimbolo.Add(sim);
+            }
+
+            ViewBag.CountU = objEjercicio11.Uentero.Count();
+            ViewData["Usimbolo"] = objEjercicio11.Usimbolo;
+            ViewData["Ullman"] = objEjercicio11.Uentero;
+
+            return View(objEjercicio11);
+        }
+    }
+}
---
+++ Ejer_Propuestos1_Catalan/Ejer_Propuestos1_Catalan/Models/ClsEjercicio11.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ejer_Propuestos1_Catalan.Models
+{
+    public class ClsEjercicio11
+    {
+        public List<int> Uentero { get; set; }
+        [Required]
+        [StringLength(1)]
+        public string simbolo { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int entero { get; set; }
+        public List<string> Usimbolo { get; set; }
+    }
+}
