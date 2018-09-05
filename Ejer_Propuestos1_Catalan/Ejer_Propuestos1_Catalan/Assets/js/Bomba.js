d0313e7610200e9cabbc9b32a52a0df52618fadc
Enzo Catalán Vargas <enzocv96@gmail.com>
2018-09-05 15:31:58 -0500

    Ejercicio 12

---
+++ Ejer_Propuestos1_Catalan/Ejer_Propuestos1_Catalan/Assets/js/Bomba.js
@@ -0,0 +1,15 @@
+﻿var MovRaton = 0
+var MovRaton2 = 0
+
+function CambiaPosicion() {
+    MovRaton = (MovRaton + 1) % 10
+    if (MovRaton == 0) {
+        Boton.style.top = window.event.y - Math.random() * 30 - Boton.clientHeight - 10
+        Boton.style.left = window.event.x - Math.random() * 30 - Boton.clientWidth - 10
+    }
+    MovRaton2 = (MovRaton2 + 1) % 10
+    if (MovRaton2 == 0) {
+        Boton2.style.top = window.event.x - Math.random() * 30 - Boton2.clientHeight - 10
+        Boton2.style.left = window.event.y - Math.random() * 30 - Boton2.clientWidth - 10
+    }
+}
---
+++ Ejer_Propuestos1_Catalan/Ejer_Propuestos1_Catalan/Controllers/Ejercicio12Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejer_Propuestos1_Catalan.Controllers
+{
+    public class Ejercicio12Controller : Controller
+    {
+        // GET: Ejercicio12
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
