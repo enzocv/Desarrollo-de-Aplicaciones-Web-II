d65bf7a7147277a77febedbd622e2629c45b2ea0
Enzo Catalán Vargas <enzocv96@gmail.com>
2018-09-09 13:29:34 -0500

    creacion de archivo JSON

--- TrabajoUnidadI_TiconaCatalan/TrabajoUnidadI_TiconaCatalan/Controllers/ConcursoController.cs
+++ TrabajoUnidadI_TiconaCatalan/TrabajoUnidadI_TiconaCatalan/Controllers/ConcursoController.cs
@@ -16,8 +16,30 @@ namespace TrabajoUnidadI_TiconaCatalan.Controllers
         // GET: Concurso
         public ActionResult Index(ClsConcurso objconcurso)
         {
-            
-            
+            objconcurso.integrantes = new List<string>();
+
+            objconcurso.tema = "proyecto 1";
+            objconcurso.curso = "LP3";
+            objconcurso.categoria = "A";
+            objconcurso.asesor = "Ing Lanchipa";
+            objconcurso.integrantes.Add("AL 1 V");
+            objconcurso.integrantes.Add("AL 2 VI");
+
+            //crear archivo JSON
+            try
+            {
+                string output = JsonConvert.SerializeObject(objconcurso, Formatting.Indented);
+
+                using (var writer = new StreamWriter(_path))
+                {
+                    writer.Write(output);
+                }
+            }
+            catch (Exception ex)
+            {
+                // ignored
+            }
+
             return View(objconcurso);
         }
     }
