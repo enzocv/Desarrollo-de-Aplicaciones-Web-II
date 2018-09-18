53b4dbd047803a96d4b6d7cc0e4326239afbfcf5
Enzo Catalán Vargas <enzocv96@gmail.com>
2018-09-18 11:53:49 -0500

    agregado controlador para realizar sorteo

---
+++ TrabajoUnidadI_TiconaCatalan/TrabajoUnidadI_TiconaCatalan/Controllers/SortearController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrabajoUnidadI_TiconaCatalan.Controllers
+{
+    public class SortearController : Controller
+    {
+        // GET: Sortear
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
